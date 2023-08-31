cc28ad4412cb9dc8946d0adae05828347dd5495a
MaximKorchagin <Airfallsc2@gmail.com>
2023-08-31 13:48:30 +0300

    init commit

---
+++ src/main/java/org/example/OnlineShop.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.util.List;
+
+public class OnlineShop {
+    private final static String shopName = "My marketplace";
+
+    private List<Product> productList;
+}
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Product {
+
+    private String productName;
+    private double price;
+    private int vendorCode;
+
+}
