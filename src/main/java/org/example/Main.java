9fe15d33d64e9c2056c9a966c45444a021021733
MaximKorchagin <Airfallsc2@gmail.com>
2023-08-31 20:21:53 +0300

    json -> products arraylist

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Product> productList = getProductsFromJson("src/main/resources/productsInfo.json");
+        System.out.println(productList);
+    }
 
+    public static List<Product> getProductsFromJson(String filePath) {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(filePath)) {
+            Object obj = jsonParser.parse(reader);
+            JSONArray products = (JSONArray) obj;
+            Gson gson = new Gson();
+            Type listType = new TypeToken<List<Product>>() {}.getType();
+            return gson.fromJson(products.toString(), listType);
+        } catch (ParseException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,9 +1,70 @@
 package org.example;
 
 public class Product {
+    private enum productType {COMPUTER_PART, HOUSEHOLD_PRODUCT, CLOTH, SPORT_PRODUCT}
+
+    public Product(String productName, int price, int vendorCode, double rating, productType type) {
+        this.productName = productName;
+        this.price = price;
+        this.vendorCode = vendorCode;
+        this.rating = rating;
+        this.type = type;
+    }
 
     private String productName;
-    private double price;
+    private int price;
     private int vendorCode;
+    private double rating;
+    productType type;
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getVendorCode() {
+        return vendorCode;
+    }
+
+    public void setVendorCode(int vendorCode) {
+        this.vendorCode = vendorCode;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public productType getType() {
+        return type;
+    }
+
+    public void setType(productType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                ", vendorCode=" + vendorCode +
+                ", rating=" + rating +
+                ", productType=" + type +
+                '}';
+    }
 }
