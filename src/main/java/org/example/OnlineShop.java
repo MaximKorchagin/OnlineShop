2bdb11e3c896777a790b59ed2d05c79a93f3a7df
MaximKorchagin <Airfallsc2@gmail.com>
2023-09-06 17:02:54 +0300

    small refactoring

--- src/main/java/org/example/OnlineShop.java
+++ src/main/java/org/example/OnlineShop.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class OnlineShop {
     private final static String shopName = "My marketplace";
     ProductsJsonParser productsJsonParser = new ProductsJsonParser();
-
     private final List<Product> productList = productsJsonParser.getProductsFromJson("src/main/resources/productsInfo.json");
 
     public List<Product> getProductList() {
